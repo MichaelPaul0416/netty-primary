961cd495a223dd0e4d00e1eb046e0ff9a3454a58
MichaelPaul0416 <18357903752m0@sina.cn>
2017-08-06 17:18:30 +0800

    【paul】创建工程

---
+++ src/main/java/com/hundsun/learn/wq/netty/traditional/Server.java
@@ -0,0 +1,12 @@
+package com.hundsun.learn.wq.netty.traditional;
+
+/**
+ * @Author:wangqiang20995
+ * @description:
+ * @Date:2017/8/6
+ */
+public class Server {
+    public static void main(String args[]){
+        System.out.println("This is Server ");
+    }
+}
