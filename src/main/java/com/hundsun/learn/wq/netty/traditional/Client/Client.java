2c6fcde4b4a79ab0fe48ebc1033bd69e322774cf
MichaelPaul0416 <18357903752m0@sina.cn>
2017-08-06 21:43:37 +0800

    【paul】同步阻塞，单线程执行和在线程池中执行

--- src/main/java/com/hundsun/learn/wq/netty/traditional/Client/Client.java
+++ src/main/java/com/hundsun/learn/wq/netty/traditional/Client/Client.java
@@ -1,12 +1,58 @@
 package com.hundsun.learn.wq.netty.traditional.Client;
 
+import com.hundsun.learn.wq.netty.traditional.base.AbstractObject;
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  * @Author:wangqiang20995
  * @description:
  * @Date:2017/8/6
  */
-public class Client {
+public class Client extends AbstractObject{
+    private static Logger logger = Logger.getLogger(Client.class);
+
     public static void main(String args[]){
-        System.out.println("This is Client connected to Server");
+        BufferedReader in = null;
+        PrintWriter out = null;
+        Socket socket = null;
+        try {
+            int port = checkPort(args,8080);
+            socket = new Socket("127.0.0.1",port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+           /* out = new PrintWriter(socket.getOutputStream());
+            out.println("QUERY_CURRENT_TIME");
+            out.flush();*/
+//            或者采用下面的方法
+            out = new PrintWriter(socket.getOutputStream(),true);
+            out.println("QUERY_CURRENT_TIME");
+            out.flush();
+
+            logger.info("send time_query command succeed");
+            String response = in.readLine();
+            logger.info("the time server response message : "+response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            out.close();
+            out = null;
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            in = null;
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            socket = null;
+        }
     }
 }
--- src/main/java/com/hundsun/learn/wq/netty/traditional/Server/Server.java
+++ src/main/java/com/hundsun/learn/wq/netty/traditional/Server/Server.java
@@ -1,12 +1,66 @@
 package com.hundsun.learn.wq.netty.traditional.Server;
 
+import com.hundsun.learn.wq.netty.traditional.base.AbstractObject;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @Author:wangqiang20995
- * @description:
+ * @description:BIO服务器端
  * @Date:2017/8/6
  */
-public class Server {
+public class Server extends AbstractObject{
+    private static Logger logger = Logger.getLogger(Server.class);
     public static void main(String args[]){
-        System.out.println("This is Server for Client");
+        int port = 8080;
+
+        checkPort(args,port);
+
+        ServerSocket serverSocket = null;
+        try {
+
+            serverSocket = new ServerSocket(port);
+            //阻塞线程执行
+            //executeByThread(serverSocket,port);
+            //线程池执行
+            executeInPool(serverSocket,port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(serverSocket != null) {
+                    logger.info("server will be closed...");
+                    serverSocket.close();
+                    serverSocket = null;
+                }
+            } catch (IOException e) {
+                logger.error("server close failed... and the message is["+e.getMessage()+"]");
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private static void executeByThread(ServerSocket serverSocket,int port) throws IOException {
+        Socket socket ;
+        logger.info("server started and the port listened is ["+port+"]");
+        while(true){
+            socket = serverSocket.accept();
+            Thread customer = new Thread(new TimeHandler(socket));
+            customer.start();
+        }
+    }
+    private static void executeInPool(ServerSocket serverSocket,int port) throws IOException {
+
+        Socket socket;
+        TimeHandlerExecutor executor = new TimeHandlerExecutor(50,10000);
+        logger.info("server started and the port listened is ["+port+"]");
+        while (true){
+            socket = serverSocket.accept();
+            executor.execute(new TimeHandler(socket));
+        }
     }
 }
